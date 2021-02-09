c58fd8803311d04731ce7e9529cd81c3c3087397
Atish Gosavi <agosavi@anant.saama.com>
2021-02-09 14:58:12 +0530

    202ctms data

---
+++ TAS120_202/layer5/studymilestone.sql
@@ -0,0 +1,45 @@
+/*
+CCDM StudyMilestone mapping
+Notes: Standard mapping to CCDM StudyMilestone table
+*/
+
+WITH included_studies AS (
+                SELECT studyid FROM study ),
+	
+     studymilestone_data AS (
+     select sm.studyid,
+     row_number() over(partition by studyid order by expecteddate) as milestoneseq,
+     sm.milestonelabel,
+     milestonetype,
+     sm.expecteddate,
+     ismandatory,
+     iscriticalpath
+     from
+     (
+                SELECT  'TAS120_202'::text AS studyid,
+                        null::int AS milestoneseq,
+                        sm."type"::text AS milestonelabel,
+                        'Planned'::text AS milestonetype,
+                        nullif(sm."planned_date",'')::date AS expecteddate,
+                        'yes'::boolean AS ismandatory,
+                        'yes'::boolean AS iscriticalpath
+                        from tas120_202_ctms.milestone sm
+                       
+                        )sm  where expecteddate is not null
+                       
+                        )
+
+SELECT 
+        /*KEY sm.studyid::text AS comprehendid, KEY*/
+        sm.studyid::text AS studyid,
+        sm.milestoneseq::int AS milestoneseq,
+        sm.milestonelabel::text AS milestonelabel,
+        sm.milestonetype::text AS milestonetype,
+        sm.expecteddate::date AS expecteddate,
+        sm.ismandatory::boolean AS ismandatory,
+        sm.iscriticalpath::boolean AS iscriticalpath
+        /*KEY , (sm.studyid || '~' || sm.milestoneType || '~' || sm.milestoneSeq)::text  AS objectuniquekey KEY*/
+        /*KEY , now()::timestamp with time zone AS comprehend_update_time KEY*/
+FROM studymilestone_data sm
+JOIN included_studies st ON (sm.studyid = st.studyid);
+
---
+++ TAS120_202/layer5/studyplannedrecruitment.sql
@@ -0,0 +1,53 @@
+/*
+CCDM studyplannedrecruitment mapping
+Notes: Standard mapping to CCDM studyplannedrecruitment table
+*/
+
+WITH included_studies AS (
+                SELECT studyid FROM study ),
+                
+     studyplannedrecruitment_data AS (
+                SELECT  'TAS120_202'::text AS studyid,
+                        'Enrollment'::text AS category,
+                        'Monthly'::text AS frequency,
+                       max("ENRDAT")::date AS enddate,
+                        'Planned'::text AS type,
+                        count("ENRYN") ::int AS recruitmentcount
+              From tas120_202."ENR"--,  Subject_count sc
+			   where "ENRYN" = 'Yes'
+               
+               union all
+                   SELECT  replace("protocol_#",'-','_')::text AS studyid,
+                        'Site Activation'::text AS category,
+                        'Monthly'::text AS frequency,
+                        max("planned_date")::date AS enddate,
+                        'Planned'::text AS type,
+                        count("visit_status") ::int AS recruitmentcount
+            From tas120_202_ctms.monvisit_tracker
+			where "visit_status" = 'Planned'
+             group by "protocol_#"
+			 union all
+			  SELECT  'TAS120_202'::text AS studyid,
+                        'Screening'::text AS category,
+                        'Monthly'::text AS frequency,
+                        max(COALESCE("MinCreated" ,"RecordDate"))::date AS enddate,
+                        'Planned'::text AS type,
+                        count("IEYN") ::int AS recruitmentcount
+                     From tas120_202."IE"
+            where trim("IEYN")='Yes'
+                )
+
+SELECT
+        /*KEY spr.studyid::text AS comprehendid, KEY*/
+        spr.studyid::text AS studyid,
+        spr.category::text AS category,
+        spr.frequency::text AS frequency,
+        spr.enddate::date AS enddate,
+        spr.type::text AS type,
+        spr.recruitmentcount::int AS recruitmentcount
+        /*KEY ,(spr.studyid || '~' || spr.category || '~' || spr.frequency || '~' || spr.enddate || '~' || spr.type)::text  AS objectuniquekey KEY*/
+        /*KEY , now()::timestamp with time zone AS comprehend_update_time KEY*/
+FROM studyplannedrecruitment_data spr
+JOIN included_studies st ON (st.studyid = spr.studyid); 
+
+
